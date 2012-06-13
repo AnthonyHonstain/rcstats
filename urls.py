2e227b624eb692dd1ef02666766dc45ca61b22ce
iteratorrc <honstain@gmail.com>
2012-06-12 17:39:43 -0700

    Updating urls and layout so dev server is the same as the prod server

--- urls.py
+++ urls.py
@@ -7,23 +7,23 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^rcdata/$', 'rcdata.views.index'),
+    url(r'^$', 'rcdata.views.index'),
     
-    url(r'^rcdata/faq$', 'faq.views.faq'),
+    url(r'^faq$', 'faq.views.faq'),
     
-    url(r'^rcdata/trackdata/$', 'trackdata.views.trackdata'),    
-    url(r'^rcdata/trackdata/(?P<track_id>\d+)/$', 'trackdata.views.trackdetail'),
-    url(r'^rcdata/trackdata/(?P<track_id>\d+)/(?P<time_frame>month|6months|alltime)/$', 'trackdata.views.trackdetail_data'),    
+    url(r'^trackdata/$', 'trackdata.views.trackdata'),    
+    url(r'^trackdata/(?P<track_id>\d+)/$', 'trackdata.views.trackdetail'),
+    url(r'^trackdata/(?P<track_id>\d+)/(?P<time_frame>month|6months|alltime)/$', 'trackdata.views.trackdetail_data'),    
     
-    url(r'^rcdata/trackdata/(?P<track_id>\d+)/recentresultshistory/$', 'trackdata.views.recentresultshistory'),
-    url(r'^rcdata/trackdata/(?P<track_id>\d+)/recentresultshistory/(?P<race_date>\d{4}-\d{1,2}-\d{2})/$', 'trackdata.views.recentresultshistory_data'),
+    url(r'^trackdata/(?P<track_id>\d+)/recentresultshistory/$', 'trackdata.views.recentresultshistory'),
+    url(r'^trackdata/(?P<track_id>\d+)/recentresultshistory/(?P<race_date>\d{4}-\d{1,2}-\d{2})/$', 'trackdata.views.recentresultshistory_data'),
     
-    url(r'^rcdata/myresults/$', 'myresults.views.myresults'),
-    url(r'^rcdata/myresults/(?P<racer_id>\d+)/$', 'myresults.views.generalstats'),
+    url(r'^myresults/$', 'myresults.views.myresults'),
+    url(r'^myresults/(?P<racer_id>\d+)/$', 'myresults.views.generalstats'),
         
-    url(r'^rcdata/displayresults/(?P<race_detail>\d+)/$', 'displayresults.views.singleraceresult'),
+    url(r'^displayresults/(?P<race_detail>\d+)/$', 'displayresults.views.singleraceresult'),
     
-    url(r'^rcdata/displayresults/singleracerlaps/(?P<race_detail>\d+)/(?P<racer_id>\d+)/$', 'displayresults.views.singleracerlaps'),
+    url(r'^displayresults/singleracerlaps/(?P<race_detail>\d+)/(?P<racer_id>\d+)/$', 'displayresults.views.singleracerlaps'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
