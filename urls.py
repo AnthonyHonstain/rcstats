0d779e244723a635b742e479400d4527c247fd86
iteratorrc <honstain@gmail.com>
2013-02-11 16:58:28 +0000

    initial checkin of registration code, got the django_registration app passing unit tests

--- urls.py
+++ urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from registration.views import activate
+from registration.views import register
 
 from django.conf import settings
 
@@ -36,7 +39,32 @@ urlpatterns = patterns('',
     url(r'^displayresults/singleracerlaps/(?P<race_detail>\d+)/(?P<racer_id>\d+)/$', 'rcstats.displayresults.views.singleracerlaps'),
 
     url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^admin/', include(admin.site.urls)),    
+    url(r'^admin/', include(admin.site.urls)),  
+    
+    url(r'^activate/complete/$', direct_to_template, 
+        {'template': 'registration/activation_complete.html'}, 
+        name='registration_activation_complete'),
+    # Activation keys get matched by \w+ instead of the more specific
+    # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+    # that way it can return a sensible "invalid key" message instead of a
+    # confusing 404.
+    url(r'^activate/(?P<activation_key>\w+)/$',
+       activate,
+       {'backend': 'registration.backends.default.DefaultBackend'},
+       name='registration_activate'),
+    url(r'^register/$',
+       register,
+       {'backend': 'registration.backends.default.DefaultBackend'},
+       name='registration_register'),
+    url(r'^register/complete/$',
+       direct_to_template,
+       {'template': 'registration/registration_complete.html'},
+       name='registration_complete'),
+    url(r'^register/closed/$',
+       direct_to_template,
+       {'template': 'registration/registration_closed.html'},
+       name='registration_disallowed'),
+    (r'', include('registration.auth_urls')),
 )
 
 if settings.DEBUG:
