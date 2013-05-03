8c0635c9c907a65414ef21353317c4f36fa4d1c4
iteratorrc <honstain@gmail.com>
2013-05-03 16:07:23 +0000

    Added unit tests for bad file uploads

--- uploadresults/tests/__init__.py
+++ uploadresults/tests/__init__.py
@@ -2,3 +2,4 @@ from rcstats.uploadresults.tests.general_race_uploader import *
 from rcstats.uploadresults.tests.legacy_general_race_uploader import *
 from rcstats.uploadresults.tests.singlerace import *
 from rcstats.uploadresults.tests.paceinfo_in_lap_times import *
+from rcstats.uploadresults.tests.bad_race_files import *
