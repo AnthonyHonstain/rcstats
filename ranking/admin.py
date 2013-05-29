f442794b5653f0d46d6a63fa9ae4578cfa5f08d7
iteratorrc <honstain@gmail.com>
2013-05-29 06:33:34 +0000

    Add 'process ranking' functionality to the admin panel, was only available by shell before

--- ranking/admin.py
+++ ranking/admin.py
@@ -1,9 +1,18 @@
 from rcstats.ranking.models import RankedClass, RankEvent, RankEventDetails, Ranking
+from rcstats.ranking.views import process_ranking
 from django.contrib import admin
+
+def admin_process_ranking(modeladmin, request, queryset):
+    # For every SingleRaceDetails in the queryset process the ranking
+    for ranked_class in queryset:
+        process_ranking(ranked_class)
+    
+admin_process_ranking.short_description = "Process Ranking - ONE AT A TIME"
 
 class RankedClassAdmin(admin.ModelAdmin):
     list_display = ('trackkey', 'raceclass', 'startdate', 'lastdate', 'experation', 'requiredraces')
     list_filter = ['trackkey', 'raceclass']
+    actions = [admin_process_ranking,]
 
 class RankedEventAdmin(admin.ModelAdmin):
     list_display = ('rankedclasskey', 'eventcount')
